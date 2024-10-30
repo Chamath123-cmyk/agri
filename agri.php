acfc2f53e6b5be7a006123dbb19b87dd6f080445
chamath neranjana <chamathneranjana854@gmail.com>
2024-10-30 17:17:55 +0530

    Initial Commit

---
+++ agri.php
@@ -0,0 +1,52 @@
+<?php
+include_once 'header.php';
+
+?>
+
+<h1 style="text-align: center;">Pass paper for Grade 10 students</h1>
+<h2 style="text-align: center;">Agriculture</h2>
+<br><br>
+
+<h3>Short note</h3>
+  <a href=""><button style="background-color: red;">Not available</button></a>
+  <?php
+  echo "<br>";
+  echo "<br>";
+  ?>
+
+<h3>Unit papers</h3><br>
+
+
+<style>
+    a{
+        text-decoration: none;
+        color: black;
+
+    }
+
+    a:hover{
+        text-decoration: underline;
+    }
+</style>
+
+
+<h3>1st Term test paper</h3>
+<a href="Grade-10-Agriculture-And-Food-Technology-1st-Term-Test-Paper-with-Answers-2020-Sinhala-Medium-North-western-Province.pdf"><button>North western Province with Answers 2020 Sinhala Medium</button></a><br>
+<a href="Grade-10-Agriculture-And-Food-Technology-1st-Term-Test-Paper-with-Answers-2020-Sinhala-Medium-Southern-Province.pdf"><button>Southern Province with Answers 2020 Sinhala Medium</button></a><br><br>
+
+<h3>2nd Term test paper</h3>
+    <a href="2023-Grade-10-Agriculture-And-Food-Technology-2nd-Term-Test-Paper-North-Western-Province.pdf"><button>North Western Province 2023 sinhala Medium</button></a><br>
+    <a href="Grade-10-Agriculture-And-Food-Technology-2nd-Term-Test-Paper-with-Answers-2018-Sinhala-Medium-North-western-Province.pdf"><button>North western Province with Answers 2018 Sinhala Medium</button></a><br>
+    <a href="Grade-10-Agriculture-And-Food-Technology-2nd-Term-Test-Paper-with-Answers-2019-Sinhala-Medium-North-western-Province.pdf"><button>North western Province with Answers 2019 Sinhala Medium</button></a><br>
+    <a href="Grade-10-Agriculture-And-Food-Technology-Paper-2019-2nd-Term-Test-North-Central-Province.pdf"><button>North Central Province 2019</button></a><br><br>
+<h3>3rd Term test paper</h3>
+<a href="2021-Grade-10-Agriculture-3rd-Term-Test-Paper-Southern-Province.pdf"><button>Southern Province 2021</button></a><br>
+<a href="2022-Grade-10-3rd-tem-Agri.pdf"><button>Southern Province 2022 sinhala Medium</button></a><br>
+<a href="Grade-10-Agriculture-And-Food-Technology-3rd-Term-Test-Paper-with-Answers-2020-Sinhala-Medium-North-western-Province.pdf"><button>North western Province with Answers 2020 Sinhala Medium</button></a><br>
+<a href="Grade-10-Agriculture-And-Food-Technology-3rd-Term-Test-Paper-with-Answers-2020-Sinhala-Medium-Southern-Province.pdf"><button>Southern Province with Answers 2020 Sinhala Medium </button></a>
+<a href="Grdae-10-Agri-Food-Technology-Sinhala-Medium-NWP.pdf"><button>North western Province 2023 sinhala Medium</button></a><br><br>
+
+
+<?php
+include_once '../../../footer.php';
+?>
